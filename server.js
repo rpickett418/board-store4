085669bd4996928d8454b3f114edc71b5a05bca8
Rahsaan Pickett <rpickettdev247@gmail.com>
2022-06-07 05:29:29 -0600

    fourth

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const serverStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serverStatic (path.join (__dirname, 'public')))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+const port = process.env.PORT || 5000
+
+app.listen(port)
+console.log('Serving' + port)
